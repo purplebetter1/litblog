30c2485d78e30dddcfe8c881a07d39de7286a0e4
Andrew Grablewski <booleanorama@gmail.com>
2018-03-04 23:39:37 -0500

    index page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Blog, BlogAuthor, BlogComment
+from django.views import generic
+
+def index(request):
+
+	return render(request,'index.html')
